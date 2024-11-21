f8179265191b683cab6ad271b9547e60293419d1
Six-Tail <qowlgh1215@naver.com>
2024-11-22 01:05:50 +0900

    [2024-11-22] Add LogOut Logic

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,7 +1,23 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';  // firebase_auth 임포트 추가
+import 'package:cloud_firestore/cloud_firestore.dart'; // cloud_firestore 임포트
 import 'package:flutter/foundation.dart';
 
 class UserService {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // 사용자 정보 가져오기
+  Future<Map<String, dynamic>?> getUserInfo(String uid) async {
+    try {
+      DocumentSnapshot userDoc =
+      await firestore.collection('users').doc(uid).get();
+      return userDoc.exists ? userDoc.data() as Map<String, dynamic>? : null;
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error fetching user info: $e");
+      }
+      return null;
+    }
+  }
 
   /// Firestore에 사용자 정보를 저장하는 함수
   Future<void> saveUserToFirestore(
@@ -12,10 +28,14 @@ class UserService {
       ) async {
     final userRef = FirebaseFirestore.instance.collection('users').doc(uid);
 
+    // 현재 로그인한 사용자의 이메일을 가져옵니다
+    final email = FirebaseAuth.instance.currentUser?.email; // firebase_auth 패키지 사용
+
     await userRef.set({
       'userName': name ?? '',
       'userImage': profileImage ?? '',
       'accountType': accountType,
+      'email': email ?? '',  // 이메일 값을 저장합니다. 없으면 빈 문자열
       'createdAt': FieldValue.serverTimestamp(),
     }, SetOptions(merge: true)); // merge: true 옵션 추가
 
